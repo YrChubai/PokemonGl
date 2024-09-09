2a7ea43623fcaea0794e755fe267d59d6e80c41e
YrChubai <razawchs@gmail.com>
2024-09-09 23:04:35 +0300

    Add Search and CachedList Viewmodel

--- app/src/main/java/com/example/pokemongl/pokemon_list/PokemonListViewModel.kt
+++ app/src/main/java/com/example/pokemongl/pokemon_list/PokemonListViewModel.kt
@@ -3,11 +3,20 @@ package com.example.pokemongl.pokemon_list
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.capitalize
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
+import com.example.pokemongl.data.models.PokedexListEntry
 import com.example.pokemongl.repository.PokemonRepository
+import com.example.pokemongl.ultil.Constants.PAGE_SIZE
+import com.example.pokemongl.ultil.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Locale
 import javax.inject.Inject
 
 
@@ -15,6 +24,78 @@ import javax.inject.Inject
 class PokemonListViewModel @Inject constructor(
     private  val repository: PokemonRepository
 ): ViewModel() {
+
+    private var curPage = 0
+
+    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    private var cachedPokemonList = listOf<PokedexListEntry>()
+    private var isSearchStarting = true
+    var isSeacrhing = mutableStateOf(false)
+
+    init {
+        loadPokemonPaginated()
+    }
+
+    fun searchPokemonList(query: String){
+        val listToSearch = if(isSearchStarting){
+            pokemonList.value
+        } else{
+            cachedPokemonList
+        }
+        viewModelScope.launch(Dispatchers.Default) {
+            if(query.isEmpty()){
+                pokemonList.value = cachedPokemonList
+                isSeacrhing.value = false
+                isSearchStarting =true
+            }
+            val result = listToSearch.filter{
+                it.pokemonName.contains(query.trim(), ignoreCase = true) ||
+                        it.number.toString() == query.trim()
+            }
+            if (isSearchStarting){
+                cachedPokemonList = pokemonList.value
+                isSearchStarting = false
+            }
+            pokemonList.value = result
+            isSeacrhing.value = true
+        }
+    }
+    fun loadPokemonPaginated(){
+        viewModelScope.launch {
+            isLoading.value = true
+            val result = repository.getPokemonList(PAGE_SIZE, curPage * PAGE_SIZE)
+            when(result) {
+                is Resource.Success -> {
+                    endReached.value = curPage * PAGE_SIZE >= result.data!!.count
+                    val pokedexEntries = result.data.results.mapIndexed{index, entry ->
+                        val number = if(entry.url.endsWith("/")){
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile{ it.isDigit()}
+                        }
+                        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+                        PokedexListEntry(entry.name.capitalize(Locale.ROOT), url, number.toInt())
+                    }
+                    curPage++
+
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokedexEntries
+                }
+                is Resource.Error -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+
+                }
+                else -> {}
+            }
+        }
+    }
+
 
     fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
         val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
@@ -25,4 +106,6 @@ class PokemonListViewModel @Inject constructor(
             }
         }
     }
+
+
 }
--- app/src/main/java/com/example/pokemongl/ultil/Constants.kt
+++ app/src/main/java/com/example/pokemongl/ultil/Constants.kt
@@ -3,4 +3,7 @@ package com.example.pokemongl.ultil
 object Constants {
 
     const val  BASE_URL =  "https://pokeapi.co/api/v2/"
+
+    const val PAGE_SIZE = 20
+
 }
